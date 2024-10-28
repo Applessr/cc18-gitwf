0ad757528caddd4ce7678cd7a9c4bf59ca195d54
Gunta2ps <gunkra056@hotmail.com>
2024-10-28 14:47:29 +0700

    Hi_Faris

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 const port = process.env.PORT || 8000
 app.listen(port, ()=> console.log(`Server is running on port ${port}`))
