bf1d6b032192041b843b047308efe004e53d52ea
NattapolDedruktip <dedruktip.n@gmail.com>
2024-10-28 14:47:57 +0700

    dog2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const dog = express();
+const dog2 = express();
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
